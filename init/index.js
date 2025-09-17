5dbde1c287b70bf825eeb2bef614a60ae6c1769b
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-17 17:22:50 +0500

    modifies index.js,style.css

--- init/index.js
+++ init/index.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
-const initdata = require('./data.js'); // Import sample data
-const Listing = require('../models/listing.js');
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
-// Connect to MongoDB
-const mongoURL = 'mongodb://localhost:27017/wanderlust';
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-.then(() => {console.log("MongoDB connection established");})
-.catch(err => console.log(err));
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-async function main(){
-    await mongoose.connect(mongoURL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    await Listing.insertMany(initdata.data);
-    console.log("Database initialized with sample data");
-
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
 };
+
 initDB();
