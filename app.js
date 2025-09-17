f986d44b0cc72f8c5066a1c8017ab10a4b592153
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-17 09:55:07 +0500

    feat: integrate ejs-mate for enhanced EJS templating support

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
-
+const ejsMate = require("ejs-mate");
 
 // Connect to MongoDB
 
@@ -22,6 +22,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs',ejsMate);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
