a69caa58f2ccb9a8dac6f648840ab375e62cff54
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-22 17:50:47 +0500

    add POST route to create and associate reviews with listings

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const listingSchema = require("./schema.js");
+const Review = require("./models/review.js");
 
 main()
   .then(() => {
@@ -83,6 +84,21 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }));
+
+//Reviews
+//POST route
+app.post("/listings/:id/reviews", async(req, res) =>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+
+});
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
