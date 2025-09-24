c8eed45010e291f826a9913e5c5798ddecc7ad42
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-24 18:02:06 +0500

    storing and using session info

--- classroom/server.js
+++ classroom/server.js
@@ -4,21 +4,22 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require("express-session");
 
-app.use(session({
-    secret : "mysupersecretstring", 
-    resave:false, 
-    saveUninitialized: true,
-}));
+const sessionOptions = {
+       secret : "mysupersecretstring", 
+       resave:false, 
+       saveUninitialized: true,
+};
 
-app.get("/reqcount", (req,res)=>{
-    if(req.session.count){
-        req.session.count++;
-    } else{
-        req.session.count =1;
-    }
-    res.send(`you sent a request ${req.session.count} times`);
+app.use(session(sessionOptions));
+
+app.get("/register", (req,res)=>{
+    let { name="anonymous" } =req.query;
+    req.session.name = name;
+    res.send(name); 
 });
-
+app.get("/hello",(req,res)=>{
+    res.send(`hello, ${req.session.name}`);
+});
 app.listen(3000, () =>{
     console.log("server is lisening to 3000");
 });
