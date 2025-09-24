a4e4323bf3887dc2256cbaa46de683818ba2b9ed
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-24 12:47:48 +0500

    restructuring reviews

---
+++ classroom/routes/post.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+//Index - post
+router.get("/", (req, res)=>{
+    res.send("get for  posts");
+});
+
+//show
+router.get("/:id", (req, res)=>{
+    res.send("show posts");
+});
+
+//post
+router.post("/", (req, res)=>{
+    res.send("post for posts");
+});
+
+//delete
+router.delete("/:id", (req, res)=>{
+    res.send("delete for posts");
+});
+
+module.exports = router;
---
+++ classroom/routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+//Index - users
+router.get("/", (req, res)=>{
+    res.send("get for  users");
+});
+
+//show
+router.get("/:id", (req, res)=>{
+    res.send("show users");
+});
+
+//post
+router.post("/", (req, res)=>{
+    res.send("post for users");
+});
+
+//delete
+router.delete("/:id", (req, res)=>{
+    res.send("delete for users");
+});
+
+module.exports = router;
