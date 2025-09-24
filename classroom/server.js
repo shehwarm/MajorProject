e29a8df02f3225a50ea1e57fd6898b7e6572f92b
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-24 17:24:13 +0500

    exploring session option

--- classroom/server.js
+++ classroom/server.js
@@ -2,7 +2,31 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const session = require("express-session");
+
+app.use(session({
+    secret : "mysupersecretstring", 
+    resave:false, 
+    saveUninitialized: true,
+}));
+
+app.get("/reqcount", (req,res)=>{
+    if(req.session.count){
+        req.session.count++;
+    } else{
+        req.session.count =1;
+    }
+    res.send(`you sent a request ${req.session.count} times`);
+});
+
+app.listen(3000, () =>{
+    console.log("server is lisening to 3000");
+});
+
+
+/*
 const cookieParser = require("cookie-parser");
+
 
 app.use(cookieParser("secretcode"));
 
@@ -33,8 +57,4 @@ app.get("/", (req, res)=>{
 
 app.use("/users", users);
 app.use("/posts", posts);
-
-
-app.listen(3000, () =>{
-    console.log("server is lisening to 3000");
-});
+*/
