6ca0123127e0671669da12e200051be5bdc5cd6d
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-24 13:06:38 +0500

    Sending cookies

--- classroom/server.js
+++ classroom/server.js
@@ -4,6 +4,11 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 
 
+app.get("/getcookies",(req, res)=>{
+    res.cookie("greet","hello");
+    res.send("sent you some cookies");
+});
+
 app.get("/", (req, res)=>{
     res.send("im root");
 });
