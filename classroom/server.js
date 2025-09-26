431940c5e8c9edb824d4120e6c257271e167bb47
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-26 12:20:18 +0500

    using connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,11 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sessionOptions = {
        secret : "mysupersecretstring", 
@@ -11,15 +16,20 @@ const sessionOptions = {
 };
 
 app.use(session(sessionOptions));
+app.use(flash());
+
 
 app.get("/register", (req,res)=>{
     let { name="anonymous" } =req.query;
     req.session.name = name;
-    res.send(name); 
+    req.flash("succces", "user registered succesfully");
+    res.redirect("/hello"); 
 });
+
 app.get("/hello",(req,res)=>{
-    res.send(`hello, ${req.session.name}`);
+    res.render("page.ejs", {name: req.session.name, msg: req.flash("success")});
 });
+
 app.listen(3000, () =>{
     console.log("server is lisening to 3000");
 });
