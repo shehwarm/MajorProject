161a90ec4e84d0bc472aae404bfe3abaaf4a89d8
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-01 09:06:16 +0500

    Passport and UserModel

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -50,12 +53,29 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
   res.locals.success =req.flash("success");
   res.locals.error =req.flash("error");
   next();
-})
+});
 
+app.get("/demouser", async (req,res)=>{
+  let fakeUser = new User ({
+    email : "student@gmail.com",
+    username :"delta-student"
+  });
+
+  let registeredUser =  await User.register(fakeUser,"helloworld");
+  res.send(registeredUser);
+
+});
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
