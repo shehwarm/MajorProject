94b3ef70e7bb47dd7e36b758c6650bfbcf6048b7
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-01 09:31:12 +0500

    Signup User-Get

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 
 
 main()
@@ -77,8 +79,9 @@ app.get("/demouser", async (req,res)=>{
 
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/",userRouter);
 
 
 
