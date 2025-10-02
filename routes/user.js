51169ab270862c09086467a6fcabecd2101f23d2
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-02 19:20:40 +0500

    logout user

--- routes/user.js
+++ routes/user.js
@@ -33,4 +33,15 @@ router.post("/login", passport.authenticate("local", {failureRedirect:'/login', 
  res.redirect("/listings");
 });
 
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);  // now next is defined properly
+    }
+    req.flash("success", "You are logged out");
+    res.redirect("/listings");
+  });
+});
+
+
 module.exports = router;
