baaa100c5346d88ce1e0088341ca9fc89e1a782c
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-07 23:14:58 +0500

    Add image upload functionality to listings
    - Updated package.json and package-lock.json to include cloudinary, dotenv, multer, and multer-storage-cloudinary dependencies.
    - Modified listing.js to implement multer for handling image uploads.
    - Updated the new.ejs view to change the image input field to a file upload and added the necessary enctype for form submission.

--- routes/listing.js
+++ routes/listing.js
@@ -3,17 +3,22 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const ListingController = require("../controllers/listings.js");
+const multer  = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(wrapAsync(ListingController.index))
-  .post(isLoggedIn, validateListing, wrapAsync(ListingController.create));
+  .post(upload.single("Listing[image]"), (req, res) =>{
+    console.log(req.file);
+  });
 
 // New Route
 router.get("/new", isLoggedIn, (req, res) => {
   res.render("listings/new.ejs");
 });
-
+ 
 
 router
    .route("/:id")
