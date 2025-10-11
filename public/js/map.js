3aeaf25a8cb90f5c0cef537e5e9504a0fe3b5b00
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-11 18:14:09 +0500

    fix: update map center to use listing geometry coordinates and adjust variable initialization

--- public/js/map.js
+++ public/js/map.js
@@ -3,13 +3,14 @@
         const map = new mapboxgl.Map({
         container: "map", // container ID
         style :"mapbox://styles/mapbox/streets-v12", // style URL
-        center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
 
     
-    console.log(coordinates);
+    console.log(listing.geometry.coordinates);
 
      const marker = new mapboxgl.Marker({ color: 'red', rotation: 45 })
         .setLngLat(coordinates)
+        .setPopup(new mapboxgl.Popup({offset: 25})).setHTML(`<h4>${listing.location}</h4><p>Exact location provided after booking<p>`)
         .addTo(map);
